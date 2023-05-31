bf771f00e439f482826367ed5d962478b2cee362
bogo8liuk <lucaborghi99@gmail.com>
2023-05-31 12:32:45 +0200

    typestate pattern

---
+++ src/Utils/TypeState.hs
@@ -0,0 +1,59 @@
+{- Typestate pattern in Haskell. Useful to define "typed alias", namely a type `a` that is identical to a type `t`,
+but they are actually different at the eyes of the compiler. For example, the function `writeFile` has the following
+type:
+
+  writeFile :: FilePath -> String -> IO ()
+
+where `FilePath` is defined as:
+
+  type FilePath = String
+
+`FilePath` is an alias to `String`, so we can accidentally flip the arguments and for the compiler everything will be ok!
+This is very dangerous! But we can use the `Typing` type and make a definition of `FilePath` like the following:
+
+  data _FilePath
+  type FilePath = _FilePath `Typing` String
+
+Now, if we try to flip the arguments of `writeFile`, the compiler will raise an error.
+-}
+
+module Utils.TypeState
+    ( Typing(..)
+) where
+
+import Control.Monad.Fix
+
+{- Typing type `t` with `a` instead of aliasing `a` as `t`. -}
+newtype Typing a t = Typing { unTyping :: t }
+
+instance Eq t => Eq (a `Typing` t) where
+    (==) (Typing x) (Typing y) = x == y
+
+instance Ord t => Ord (a `Typing` t) where
+    compare (Typing x) (Typing y) = compare x y
+
+instance Semigroup t => Semigroup (a `Typing` t) where
+    (<>) (Typing x) (Typing y) = Typing $ x <> y
+
+instance Monoid t => Monoid (a `Typing` t) where
+    mempty = Typing mempty
+
+instance Show t => Show (a `Typing` t) where
+    show (Typing x) = show x
+
+deriving instance Read t => Read (a `Typing` t)
+
+instance Functor (Typing a) where
+    fmap f (Typing x) = Typing $ f x
+
+instance Applicative (Typing a) where
+    pure = Typing
+
+    (<*>) (Typing f) (Typing x) = Typing $ f x
+
+instance Monad (Typing a) where
+    (>>=) (Typing x) op = op x
+
+instance MonadFix (Typing a) where
+    mfix f =
+        let x = f (unTyping x) in x
