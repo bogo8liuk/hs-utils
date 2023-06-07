65fd097c894856fd8182aca8003d8620d7c8c038
bogo8liuk <lucaborghi99@gmail.com>
2023-06-07 11:55:13 +0200

    instances for KnowledgeOneOf and updating KnowledgeOf instances

--- src/Utils/Data/Knowledge.hs
+++ src/Utils/Data/Knowledge.hs
@@ -15,8 +15,8 @@ module Utils.Data.Knowledge
     , isNo
     , isDon'tKnow
     , KnowledgeOneOf(..)
-    , isThis
-    , isThat
+    , isError
+    , isOk
     , isNone
 ) where
 
@@ -30,27 +30,37 @@ instance Semigroup a => Semigroup (KnowledgeOf a) where
     (<>) x Don'tKnow = x
     (<>) (Yes x) (Yes y) = Yes (x <> y)
     (<>) (Yes x) (No y) = Yes (x <> y)
-    (<>) (No x) (Yes y) = No (x <> y)
+    (<>) (No x) (Yes y) = Yes (x <> y)
     (<>) (No x) (No y) = No (x <> y)
 
 instance Functor KnowledgeOf where
     fmap f (Yes x) = Yes $ f x
+    {- |
+    Here, there's the main particularity of this type: the fact that also `No` constructor is mapped. Note that
+    if `KnowledgeOf` was an alias of `KnowledgeOneOf`, it wouldn't be possible to map `No` constructor.
+    -}
     fmap f (No x) = No $ f x
     fmap _ Don'tKnow = Don'tKnow
 
 instance Applicative KnowledgeOf where
     pure = Yes
 
+    {- |
+    Look at @Functor@ instance to see the note about `No` costructor mapping.
+    -}
     (<*>) Don'tKnow _ = Don'tKnow
     (<*>) _ Don'tKnow = Don'tKnow
     (<*>) (Yes f) (Yes x) = Yes $ f x
-    (<*>) (Yes f) (No x) = Yes $ f x
+    (<*>) (Yes f) (No x) = No $ f x
     (<*>) (No f) (Yes x) = No $ f x
     (<*>) (No f) (No x) = No $ f x
 
 instance Foldable KnowledgeOf where
     foldMap _ Don'tKnow = mempty
     foldMap f (Yes x) = f x
+    {- |
+    Here, the same thing happing also in the @Functor@ instance.
+    -}
     foldMap f (No x) = f x
 
     null Don'tKnow = True
@@ -73,16 +83,46 @@ isDon'tKnow :: KnowledgeOf a -> Bool
 isDon'tKnow Don'tKnow = True
 isDon'tKnow _ = False
 
-{- Like Maybe Either, but using a single type. -}
-data KnowledgeOneOf a b = This a | That b | None deriving (Show, Eq, Ord)
+{- |
+Like a @Maybe@ of @Either@.
+-}
+data KnowledgeOneOf e a = Error e | Ok a | None deriving (Show, Eq, Ord)
 
-isThis :: KnowledgeOneOf a b -> Bool
-isThis (This _) = True
-isThis _ = False
+instance Semigroup (KnowledgeOneOf e a) where
+    (<>) None k = k
+    (<>) k None = k
+    (<>) (Error _) k = k
+    (<>) k _ = k
 
-isThat :: KnowledgeOneOf a b -> Bool
-isThat (That _) = True
-isThat _ = False
+instance Functor (KnowledgeOneOf a) where
+    fmap _ None = None
+    fmap _ (Error e) = Error e
+    fmap f (Ok x) = Ok $ f x
+
+instance Applicative (KnowledgeOneOf a) where
+    pure = Ok
+
+    (<*>) None _ = None
+    (<*>) (Error e) _ = Error e
+    (<*>) (Ok f) x = fmap f x
+
+instance Foldable (KnowledgeOneOf a) where
+    foldMap _ None = mempty
+    foldMap _ (Error _) = mempty
+    foldMap f (Ok x) = f x
+
+instance Traversable (KnowledgeOneOf a) where
+    traverse _ None = pure None
+    traverse _ (Error e) = pure $ Error e
+    traverse f (Ok x) = Ok <$> f x
+
+isError :: KnowledgeOneOf a b -> Bool
+isError (Error _) = True
+isError _ = False
+
+isOk :: KnowledgeOneOf a b -> Bool
+isOk (Ok _) = True
+isOk _ = False
 
 isNone :: KnowledgeOneOf a b -> Bool
 isNone None = True
